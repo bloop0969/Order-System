ec3afb83d09338fa3eea522cdf0849e2df5b2bce
bloop0969 <150101583+bloop0969@users.noreply.github.com>
2024-12-07 18:24:07 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import OrderSystem from './components/OrderSystem'
+
+function App() {
+  return (
+    <div className="App">
+      <OrderSystem />
+    </div>
+  )
+}
+
+export default App
+
